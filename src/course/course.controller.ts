a5e00fe1a2075df48bb29ddd97131e1c5449889a
yehanna <hannayehuala@gmail.com>
2023-02-10 19:50:21 +0300

    getting a course with id

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,16 +1,4 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  ParseIntPipe,
-  Patch,
-  Post,
-  Put,
-  Req,
-  UseGuards,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Put, Req, UseGuards } from '@nestjs/common';
 import { CourseService } from './course.service';
 import { timeStamp } from 'console';
 import { AuthGuard } from '@nestjs/passport';
@@ -19,39 +7,38 @@ import { Request } from 'express';
 
 @Controller('courses')
 export class CourseController {
-  constructor(private courseService: CourseService) {}
+    constructor(private courseService: CourseService) {}
 
-  @Get()
-  getAllCources() {
-    return this.courseService.getAllCourses();
-  }
+    @Get()
+    getAllCources() {
+        return this. courseService.getAllCourses()
+    }
 
-  @UseGuards(AuthGuard('jwt'))
-  @Post()
-  async createCourse(@Body() dto: CourseDto, @Req() req: Request) {
-    return await this.courseService.createCourse(dto, req.user['id']);
-  }
+    @Get(':id')
+    async getCourse(@Param('id', new ParseIntPipe()) courseId: number) {
+        return await this.courseService.getCourseById(courseId)
+    }
 
-  /**
-   * a controller to update only the name or description of a course. it must be the author
-   * who sends the update request or it won't work.
-   */
-  @UseGuards(AuthGuard('jwt'))
-  @Patch(':id')
-  async updateCourse(
-    @Param('id', new ParseIntPipe()) courseId: number,
-    @Req() req: Request,
-    @Body() dto: CourseDto,
-  ) {
-    return await this.courseService.updateCourse(courseId, req.user, dto);
-  }
+    @UseGuards(AuthGuard("jwt"))
+    @Post()
+    async createCourse(@Body() dto: CourseDto, @Req() req: Request) {
+        return await this.courseService.createCourse(dto, req.user['id'])
+    }
 
-  @UseGuards(AuthGuard('jwt'))
-  @Delete(':id')
-  async deleteCourse(
-    @Param('id', new ParseIntPipe()) courseId: number,
-    @Req() req: Request,
-  ) {
-    return await this.courseService.deleteCourse(courseId, req.user);
-  }
+
+    /**
+     * a controller to update only the name or description of a course. it must be the author
+     * who sends the update request or it won't work.
+     */
+    @UseGuards(AuthGuard("jwt"))
+    @Patch(':id')
+    async updateCourse(@Param('id', new ParseIntPipe()) courseId: number, @Req() req: Request, @Body() dto: CourseDto) {
+        return await this.courseService.updateCourse(courseId, req.user, dto)
+    }
+
+    @UseGuards(AuthGuard("jwt"))
+    @Delete(':id')
+    async deleteCourse(@Param('id', new ParseIntPipe()) courseId: number, @Req() req: Request){
+        return await this.courseService.deleteCourse(courseId, req.user)
+    }
 }
